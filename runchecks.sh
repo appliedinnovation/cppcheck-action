1c8bddd2b4cb46956708b7073d44469e73dbde60
Pranav <pranav.srinivas.kumar@gmail.com>
2021-06-11 18:31:19 -0500

    Update runchecks.sh

--- runchecks.sh
+++ runchecks.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+# Checkout master and the PR commit
 git fetch origin master 
 git checkout master
 git fetch origin $1
 git checkout $1
+
 echo "Performing cppcheck"
 COMMIT=$1
 echo $COMMIT
-FILES=`git --no-pager diff --name-only $COMMIT..master`
+
+# Get list of changed .cpp files
+git --no-pager diff --name-only $COMMIT..master > changes.txt
+FILES=`cat changes.txt | grep *.cpp`
 echo $FILES
 
 if [ -n "$FILES" ]; then
     cppcheck -v --enable=all --language=c++ --template="::{severity} file={file},line={line},col={column}::{message}" --error-exitcode=1 $FILES
-else
-    cppcheck -v --enable=all --language=c++ --template="::{severity} file={file},line={line},col={column}::{message}" --error-exitcode=1 .
 fi
