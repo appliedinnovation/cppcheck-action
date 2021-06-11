f09a9abf24800b782400c4bd23623dba7730a772
Pranav <pranav.srinivas.kumar@gmail.com>
2021-06-11 12:35:14 -0500

    Update runchecks.sh

--- runchecks.sh
+++ runchecks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Performing checkup:"
-cppcheck $2 --enable=all --language=c++ --template="::{severity} file={file},line={line},col={column}::{message}" $1
+cppcheck --enable=all --language=c++ --template="::{severity} file={file},line={line},col={column}::{message}" $1
