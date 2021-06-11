4b872cb179cd4ccb2d4fbd4ffb3c415ba044c8f2
Pranav <pranav.srinivas.kumar@gmail.com>
2021-06-11 13:03:04 -0500

    Update runchecks.sh

--- runchecks.sh
+++ runchecks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Performing cppcheck"
-cppcheck --enable=all --language=c++ --template="::{severity} file={file},line={line},col={column}::{message}" .
+cppcheck -v --enable=all --language=c++ --template="::{severity} file={file},line={line},col={column}::{message}" --error-exitcode=1 .
