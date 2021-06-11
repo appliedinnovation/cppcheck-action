0c7fed857e2360661a69f2047c6aed620d8d41da
Pranav <pranav.srinivas.kumar@gmail.com>
2021-06-11 17:49:22 -0500

    Update runchecks.sh

--- runchecks.sh
+++ runchecks.sh
@@ -3,6 +3,6 @@
 echo "Performing cppcheck"
 COMMIT=$1
 echo $COMMIT
-FILES=`git --no-pager diff --name-only master...$COMMIT`
+FILES=`git --no-pager diff --name-only $COMMIT..master`
 echo $FILES
 cppcheck -v --enable=all --language=c++ --template="::{severity} file={file},line={line},col={column}::{message}" --error-exitcode=1 $FILES
