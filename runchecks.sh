ede3a509b8b5d88c6107880422ad53c3a749e29c
Pranav <pranav.srinivas.kumar@gmail.com>
2021-06-11 14:10:32 -0500

    Run only on files diff between HEAD and master

--- runchecks.sh
+++ runchecks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Performing cppcheck"
-cppcheck -v --enable=all --language=c++ --template="::{severity} file={file},line={line},col={column}::{message}" --error-exitcode=1 .
+cppcheck -v --enable=all --language=c++ --template="::{severity} file={file},line={line},col={column}::{message}" --error-exitcode=1 `git --no-pager diff --name-only master...HEAD`
